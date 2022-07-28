482a3b9100db63a1687282173a1feb7bf7229d94
Abdelrahman-Moubarak <57411478+Abdelrahman-Moubarak@users.noreply.github.com>
2022-07-28 20:41:36 +0200

    changes for deployment

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const path = require('path');
 
 const app = express();
 
+// add this code
+const whitelist = ['http://localhost:3000', 'https://file-uploader-spring-app.herokuapp.com']; // list of allow domain
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (whitelist.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}
+app.use(cors(corsOptions));
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/fileuploader-frontend'));
 
