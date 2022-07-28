045d077de0b673417e9426a09b1c30f4128c01e0
Abdelrahman-Moubarak <57411478+Abdelrahman-Moubarak@users.noreply.github.com>
2022-07-28 19:11:42 +0200

    changes for deployment

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-file-uploader'));
+app.use(express.static('./dist/fileuploader-frontend'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-file-uploader/'}),
+  res.sendFile('index.html', {root: 'dist/fileuploader-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
