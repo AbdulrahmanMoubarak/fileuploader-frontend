05158c46689a1a8c37ba972e7d20670fd989ca12
Abdelrahman-Moubarak <57411478+Abdelrahman-Moubarak@users.noreply.github.com>
2022-07-29 23:56:43 +0200

    changes for deployment

--- src/app/upload.service.ts
+++ src/app/upload.service.ts
@@ -9,13 +9,23 @@ export class UploadService {
 
   private baseURL = 'https://file-uploader-spring-app.herokuapp.com/';
 
-  private httpHeaders = {
+  private puthttpHeaders = {
     'Access-Control-Allow-Origin': '*',
     'reportProgress': 'true',
     'responseType': 'json',
     'observe': 'response',
   };
-  private httpOptions = {headers: new HttpHeaders(this.httpHeaders)};
+  private puthttpOptions = {headers: new HttpHeaders(this.puthttpHeaders)};
+
+  private posthttpHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'reportProgress': 'true',
+    'responseType': 'json',
+    'observe': 'response',
+    'Content-Type': 'multipart/form-data'
+  };
+  private posthttpOptions = {headers: new HttpHeaders(this.posthttpHeaders)};
+
 
   constructor(private client: HttpClient) {
   }
@@ -24,7 +34,7 @@ export class UploadService {
     console.log("Service Started")
     let formdata = new FormData();
     formdata.append("file", file)
-    return this.client.post<any>(this.baseURL + 'upload', formdata, this.httpOptions);
+    return this.client.post<any>(this.baseURL + 'upload', formdata, this.posthttpOptions);
   }
 
   editMaxFileSize(mSize: string): Observable<HttpResponse<any>> {
@@ -32,6 +42,6 @@ export class UploadService {
     console.log(this.baseURL+"setMaxSize")
     let formdata = new FormData();
     formdata.append("size", mSize)
-    return this.client.put<HttpResponse<any>>(this.baseURL + 'setMaxSize', formdata, this.httpOptions);
+    return this.client.put<HttpResponse<any>>(this.baseURL + 'setMaxSize', formdata, this.puthttpOptions);
   }
 }
