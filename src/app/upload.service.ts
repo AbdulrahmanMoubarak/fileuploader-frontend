1324401823e7c828dfff713b2104f395da25deb6
Abdelrahman-Moubarak <57411478+Abdelrahman-Moubarak@users.noreply.github.com>
2022-07-29 22:55:04 +0200

    changes for deployment

--- src/app/upload.service.ts
+++ src/app/upload.service.ts
@@ -14,7 +14,7 @@ export class UploadService {
     "Access-Control-Allow-Headers": "Origin, X-Requested, Content-Type, Accept Authorization",
     "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE",
     'Access-Control-Allow-Credentials': 'true',
-    'Accept':"*/*",
+    'Accept': "*/*",
     "Content-Type": "multipart/form-data",
   };
   private httpOptions = {headers: new HttpHeaders(this.httpHeaders)};
@@ -26,12 +26,20 @@ export class UploadService {
     console.log("Service Started")
     let formdata = new FormData();
     formdata.append("file", file)
-    return this.client.post<any>(this.baseURL + 'upload', formdata, {observe: 'response', reportProgress: true});
+    return this.client.post<any>(this.baseURL + 'upload', formdata, {
+      observe: 'response',
+      reportProgress: true,
+      'Access': "true",
+      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Headers": "Origin, X-Requested, Content-Type, Accept Authorization",
+      "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE",
+      'Access-Control-Allow-Credentials': 'true',
+    });
   }
 
   editMaxFileSize(mSize: string): Observable<HttpResponse<any>> {
     console.log("Service started");
-    console.log(this.baseURL+"setMaxSize")
+    console.log(this.baseURL + "setMaxSize")
     let formdata = new FormData();
     formdata.append("size", mSize)
     return this.client.put<HttpResponse<any>>(this.baseURL + 'setMaxSize', formdata);
